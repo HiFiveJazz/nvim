b8bbd674a2ac56fec07e8086066a258d0c19379c
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-09-22 19:11:43 -0700

    better compatibility with aerc

--- lua/user/baleia.lua
+++ lua/user/baleia.lua
@@ -1,19 +1,25 @@
 local M = {
   "m00qek/baleia.nvim",
 
+  -- aerc explicitly sets this environment variable for its Neovim viewer.
   cond = function()
     return vim.env.AERC_NVIM_VIEWER == "1"
   end,
+
+  -- Important: Baleia needs to be loaded before stdin is read.
+  lazy = false,
 }
 
 M.config = function()
-  vim.g.baleia = require("baleia").setup({
+  local baleia = require("baleia").setup({
     strip_ansi_codes = true,
     async = false,
   })
 
+  vim.g.baleia = baleia
+
   vim.api.nvim_create_user_command("BaleiaColorize", function()
-    vim.g.baleia.once(vim.api.nvim_get_current_buf())
+    baleia.once(vim.api.nvim_get_current_buf())
   end, { bang = true })
 
   vim.api.nvim_create_user_command("BaleiaLogs", function()
@@ -21,8 +27,14 @@ M.config = function()
   end, { bang = true })
 
   vim.api.nvim_create_autocmd("StdinReadPost", {
+    once = true,
     callback = function(ev)
-      vim.g.baleia.once(ev.buf)
+      -- Extra guard: only process stdin from the aerc viewer.
+      if vim.env.AERC_NVIM_VIEWER ~= "1" then
+        return
+      end
+
+      baleia.once(ev.buf)
       vim.bo[ev.buf].modified = false
     end,
   })
